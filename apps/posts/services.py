468c9c1dcdb1ddddc6c5ee6a0902e0b000dcde1a
WanerValencia2019 <valenciawaner@gmail.com>
2021-07-29 14:20:09 -0500

    refactor services

--- apps/posts/services.py
+++ apps/posts/services.py
@@ -1,9 +1,12 @@
 import requests
-
+from .models import Post
 API_URL = "https://jsonplaceholder.typicode.com/posts"
 
 # GET ALL POST FROM API JSON PLACEHOLDER
 def get_all_post_from_api():
+    post_count = Post.objects.all().count() 
+    if post_count > 0:
+        return None       
     response = requests.get(API_URL)
     if response.status_code == 200:
         return response.json()
